874eac9cc79a465189886a9b1e6004dba2550aaa
Tuukka Wahtera <tuukka@wahtera.fi>
2024-08-28 14:33:32 +0300

    Downgrade nvidiaPackages

--- configuration.nix
+++ configuration.nix
@@ -90,7 +90,7 @@
       nvidiaSettings = true;
 
       # Optionally, you may need to select the appropriate driver version for your specific GPU.
-      package = config.boot.kernelPackages.nvidiaPackages.stable;
+      package = config.boot.kernelPackages.nvidiaPackages.legacy_535;
 
       prime = {
         sync.enable = true;
