8ea24d5edcf7f7f00594bb5705b0eb8f17f92e1d
Tuukka Wahtera <tuukka@wahtera.fi>
2024-10-13 21:30:29 +0300

    Furikawari config

--- furikawari/configuration.nix
+++ furikawari/configuration.nix
@@ -43,12 +43,20 @@
     LC_TIME = "fi_FI.UTF-8";
   };
 
+  services.xserver = {
+    enable = true;
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
+
+    windowManager.xmonad.enable = true;
+    windowManager.xmonad.enableContribAndExtras = true;
+
+  };
   # Enable the X11 windowing system.
-  services.xserver.enable = true;
+
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+
 
   # Configure keymap in X11
   services.xserver.xkb = {
@@ -87,11 +95,20 @@
     description = "Tuukka Wahtera";
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
-	git
+	    git
+      rxvt-unicode
     #  thunderbird
     ];
   };
 
+  services.syncthing = {
+    enable = true;
+    user = "twah";
+    group = "users";
+    openDefaultPorts = true;
+    configDir = "/home/twah/.config/syncthing";
+    dataDir = "/home/twah/.config/syncthing/db";
+  };
   # Install firefox.
   programs.firefox.enable = true;
 
@@ -119,18 +136,10 @@
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  services.physlock = {
+    enable = true;
+    muteKernelMessages = true;
+  };
   system.stateVersion = "24.05"; # Did you read the comment?
 
 }
